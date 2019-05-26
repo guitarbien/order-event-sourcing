b43e150c7c9702c4e589e87d94c910f3c6ff4b5e
guitarbien <guitarbien@gmail.com>
2019-05-26 13:38:42 +0800

    加入 route 和實作，測試通過

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('api')->post('/users', function (Request $request) {
+    \App\User::create($request->all());
+    return new \Illuminate\Http\JsonResponse([], 201);
+});
+
 Route::resource('orders', OrderController::class);
 // Route::get('transactions', [TransactionsController::class, 'index']);
