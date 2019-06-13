367584b65ee40c19757baf6b7a5f7a034fe155d5
guitarbien <guitarbien@gmail.com>
2019-06-13 14:26:27 +0800

    composer update spatie/laravel-event-projector

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-05-24 10:00:44.
+    * Generated on 2019-06-13 14:25:45.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
